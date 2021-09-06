97aa195c3ae1087c88fe81c0a557b67354308f1b
Aga Skorska <skorskaga@gmail.com>
2021-09-06 10:23:01 +0100

    delete comment

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
-
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' })
 const app = express();
-app.engine('hbs', hbs());
-app.set('view engine', 'hbs');
+
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
+// forms x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.post('/contact/send-message', upload.single('image'), (req, res) => {
+
+  const { author, sender, title, message } = req.body;
+
+  if(author && sender && title && message) {
+    res.render('contact', { isSent: true, fileName: req.file.originalname } );
+  }
+  else {
+    res.render('contact', { isError: true });
+  }
 });
 
 app.get('/hello/:name', (req, res) => {
@@ -17,7 +34,7 @@ app.get('/hello/:name', (req, res) => {
 });
 
 app.get('/about', (req, res) => {
-  res.render('about');
+  res.render('about.hbs', { layout: 'dark' });
 });
 
 app.get('/contact', (req, res) => {
@@ -28,7 +45,7 @@ app.get('/info', (req, res) => {
   res.render('info');
 });
 
-app.get('/history', (req, res) => {
+app.get('/history', (req, res, next) => {
   res.render('history');
 });
 
